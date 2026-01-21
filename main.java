83a9588cb87d36ed016bd429ee582c1d3d55dcd2
Pranish Karki <praniskkarki20@gmail.com>
2026-01-21 16:08:42 +0530

    Frist

---
+++ main.java
@@ -0,0 +1,10 @@
+class hello                                                                                                         
+{
+public static void  main(String a[])
+{
+    int num = 3 ;
+    int num2 = 4 ;
+    int result =  num + num2 ;
+    System.out.print(result);
+}
+}
