4af30955b374b51151e34d4e0aa74751f5cb4dbf
Pranish Karki <praniskkarki20@gmail.com>
2026-01-22 23:06:31 +0530

    casting.java

--- casting.java
+++ casting.java
@@ -4,6 +4,11 @@ public class  casting{
         byte a = (byte) b ;
         float f = 3.5f;
         int c = (int) f;
+
+        byte  e = 10;
+        byte h = 30 ; 
+        int result = e * h ; 
+        System.out.println(result);
         System.out.println(b);
         System.out.println(a);
         System.out.println(c);
