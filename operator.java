1aea74cf266209ef8934591af342d83223e4f72c
Pranish Karki <praniskkarki20@gmail.com>
2026-01-22 23:29:15 +0530

    operation

---
+++ operator.java
@@ -0,0 +1,10 @@
+ public class operator 
+ {
+    public static void main (String  a[]){
+        int x = 5 ; 
+        int y = 8 ; 
+        boolean result = x > y  ;
+        System.out.println(result) ;
+
+    }
+ }
