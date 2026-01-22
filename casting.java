e9f8615ed0e4b3573a79a04976c492f2a423c4ed
Pranish Karki <praniskkarki20@gmail.com>
2026-01-22 23:01:46 +0530

    casting java

---
+++ casting.java
@@ -0,0 +1,11 @@
+public class  casting{
+    public static void main(String[] args) {
+        int b = 257;
+        byte a = (byte) b ;
+        float f = 3.5f;
+        int c = (int) f;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+    }
+}
