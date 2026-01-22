ff4e5d2a6ef79385607ac79037b0bb7ad027c9be
Pranish Karki <praniskkarki20@gmail.com>
2026-01-22 22:05:08 +0530

    datatypes.java

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+class datatype{
+    public static void main (String a[]){
+        float num1 = 7.5f;
+        int   num2 = 6 ;
+        byte  num3 = 127;
+        long  num4 = 1344555l;
+        boolean b  = true ;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(b);
+    }
+}
