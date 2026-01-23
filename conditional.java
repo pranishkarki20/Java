054b019f0f03f5070657b5f2613f53a861e42e10
Pranish Karki <praniskkarki20@gmail.com>
2026-01-23 10:22:42 +0530

    Conditional.java

---
+++ conditional.java
@@ -0,0 +1,10 @@
+public class conditional {
+    public static void main (String a[]){
+        int x = 1 ;
+        if ( x > 10  && x <= 20 )
+        System.out.println("Heloo");
+        else 
+            System.out.println("bye");
+    }
+    
+}
