a8218eb55c58c7bcf6c12eb19de4ac3d976f3d9a
Pranish Karki <praniskkarki20@gmail.com>
2026-01-23 15:48:43 +0530

    I/o in java

--- loop.java
+++ loop.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
-public class loop  {
-    public static void main(String a[]){
+class loop{
+    public static void main (String a[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of time you want to print ");
+        System.out.println("Enter the number ");
         int n = sc.nextInt();
-        int i = 1 ;
-        do
-        {
-        System.out.println("Hi");
-        i++;
-        }while( i <= n );
+        System.out .print("You have entered " + n);
+        sc.nextLine();
+        System.out.print("Enter your full name ");
+        String name = sc.nextLine();
+        System.out.println("Name:" + name );
+        sc.nextLine();
+        System.out.println("Enter you address");
+        String address = sc.nextLine();
+        System.out.println("Address:" + address);
     }
 }
