90a65ad9525cef0f68c80c4053c4ad09f81d415b
Pranish Karki <praniskkarki20@gmail.com>
2026-02-11 08:24:35 +0530

    Practice.java

--- practice.java
+++ practice.java
@@ -1,9 +1,9 @@
 import java.util.Scanner ;
 
 public class practice{
-    public static int  add(int a , int b){
+    public static void  add(int a , int b){
         int result = a + b ;
-        return result;
+        System.out.println("The  sum of number you have entered is " + result);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,7 +11,7 @@ public class practice{
         int a = sc.nextInt();
         System.out.println("Enter the number you want to  print");
         int b = sc.nextInt();
-        System.out.println("The number you have entered is " + add(a, b));
+        add(a, b);
 
     }
 }
