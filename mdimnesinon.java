73cdc5b4d444b328a4b41391d6a21d7ed44bd7ba
pranishkarki20 <praniskkarki20@gmail.com>
2026-02-14 18:54:28 +0530

    jagged

--- mdimnesinon.java
+++ mdimnesinon.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class mdimnesinon {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[2][2];
+        int[][] arr = new int[5][ ];
         for(int i = 0 ; i < 2 ; i++){
             for (int j = 0 ; j < 2 ; j++){
                 System.out.print("Enter number for [" + i + "][" + j + "]: ");
