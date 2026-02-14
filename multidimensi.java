da769992cc35fe28eebd10bbf82d1196d94f2f2e
pranishkarki20 <praniskkarki20@gmail.com>
2026-02-14 17:47:04 +0530

    multidimesni.java

---
+++ multidimensi.java
@@ -0,0 +1,14 @@
+ class multidimensi {
+    public static void main(String[]  args){
+        int[][] matrix = new int[3][3];
+        matrix[0][0]=4;
+        matrix[0][1]=3;
+        matrix[0][2]=3;
+        /*int[][] matrix = {
+            {1 , 2 , 3},
+            {1 , 2 , 3},
+            {1 , 2 , 3}
+        };*/
+        System.out.println(matrix[0][0]);
+    }
+}
