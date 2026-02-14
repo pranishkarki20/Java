32417717af8f5a2ee77c22bfcb45d3fd81a83e0f
pranishkarki20 <praniskkarki20@gmail.com>
2026-02-14 18:53:26 +0530

    jagged

--- jagged.java
+++ jagged.java
@@ -8,17 +8,9 @@ public class jagged {
         nums[2] = new int [2];
 
         nums[0][0] = 10 ;
-        nums[0][1] = 10 ;
-        nums[0][2] = 10 ;
         nums[1][2] = 10 ;
         nums[2][0] = 10 ;
-
-        for(int i = 0 ; i < 3 ; i++){
-            for (int j = 0 ; j < 3
-                 ; j++){
-        System.out.println(nums[i][j]);
-        }
-        System.out.println();    
-}
+        
+        System.out.println(nums[1][2]);
 }
 }
